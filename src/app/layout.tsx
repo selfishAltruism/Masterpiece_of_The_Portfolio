68779493a9dfc998112ff55b0d8d78b3b20ddaa8
selfishAltruism <officialkyus@gmail.com>
2025-10-14 00:35:24 +0900

    (+) 파비콘 적용 및 모바일 over scroll 대응.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,19 +7,34 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Kyu | 강민규",
   description:
-    "프론트엔드 개발자 강민규의 개발 블로그입니다. Next.js, React, React Native 등 다양한 기술 스택을 다룹니다.",
+    "안전지대를 넘어서는 개발자, 강민규의 경력 기술 웹 페이지입니다. React, Next.js, Web Graphics를 주로 다루고 있습니다.",
   keywords: [
     "강민규",
     "프론트엔드 개발자",
     "Front-end Developer",
-    "Next.js 개발자",
     "React 개발자",
+    "Next.js 개발자",
     "React Native 개발자",
     "중앙대학교 소프트웨어학부",
     "IDIS 연구원",
     "IDIS SW 연구원",
-    "SW 연구원",
+    "웹 개발자",
   ],
+  icons: {
+    icon: [
+      { url: "/favicon/favicon.ico", sizes: "any" },
+      {
+        url: "/favicon/favicon-32x32.png",
+        type: "image/png",
+        sizes: "32x32",
+      },
+      {
+        url: "/favicon/favicon-16x16.png",
+        type: "image/png",
+        sizes: "16x16",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
