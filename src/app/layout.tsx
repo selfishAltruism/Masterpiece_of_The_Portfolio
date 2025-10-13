02a35c099531e96bdbfc7852acc8d4af14306b29
selfishAltruism <officialkyus@gmail.com>
2025-10-14 00:59:26 +0900

    (+) 파비콘 재적용 및 모바일 핀치 인 대응.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "../index.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 5,
+  userScalable: true,
+};
 
 export const metadata: Metadata = {
   title: "Kyu | 강민규",
